da495cd8031519ee8603207404cd02a8c98d5e4a
mhbackes <mhbackes@inf.ufrgs.br>
2016-04-06 17:35:14 -0300

    matrix.h

--- trabalho1/thread/src/matrix-thread.c
+++ trabalho1/thread/src/matrix-thread.c
@@ -2,19 +2,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void multiplyRowCol(int row, int col);
-int toN(int row, int col, int numCols);
-int toI(int elem, int numCols);
-int toJ(int elem, int numCols);
-int* readMatrix(char* path, int* numRows, int* numCols);
-void writeMatrix(char* path, int* M, int numRows, int numCols);
-void printMatrix(FILE* file, int *M, int numRows, int numCols);
+#include "matrix.h"
 
-int *A, *B, *C;
-int numRowsA, numColsA;
-int numRowsB, numColsB;
-int numRowsC, numColsC;
-int size;
+Matrix *A, *B, *C;
 
 int numThreads;
 
@@ -27,98 +17,32 @@ int main(int argc, char *argv[])
 		printf("Usage: matrix-thread nThreads A B [C]\n");
 		return -1;
 	}
-	A = readMatrix(argv[2], &numRowsA, &numColsA);
-	B = readMatrix(argv[3], &numRowsB, &numColsB);
-	if(numColsA != numRowsB) {
+	numThreads = atoi(argv[1]);
+	A = readMatrix(argv[2]);
+	B = readMatrix(argv[3]);
+	if(A->numCols != B->numRows) {
 		fprintf(stderr, "Wrong matrix dimensions\n");
 		return -1;
 	}
-	numRowsC = numRowsA;
-	numColsC = numColsB;
-	size = numRowsC * numColsC;
-	C = malloc(sizeof(int) * size);
-	numThreads = atoi(argv[1]);
+	C = makeMatrix(A->numRows, B->numCols);
+
 	dispatchThreads();
 
 	if(argc > 4)
-		writeMatrix(argv[4], C, numRowsC, numColsC);
+		writeMatrix(argv[4], C);
 	else
-		printMatrix(stdout, C, numRowsC, numColsC);
+		printMatrix(stdout, C);
 	return 0;
 }
 
-int toN(int row, int col, int numCols) {
-	return row * numCols + col;
-}
-
-int toI(int elem, int numCols) {
-	return elem / numCols;
-}
-
-int toJ(int elem, int numCols) {
-	return elem % numCols;
-}
-
-void multiplyRow(int row) {
-	int i;
-	for(i = 0; i < numColsB; i++) {
-		multiplyRowCol(row, i);
-	}
-}
-
-void multiplyRowCol(int row, int col) {
-	int i, sum = 0;
-	for(i = 0; i < numColsA; i++) {
-		sum += A[toN(row, i, numColsA)] * B[toN(i, col, numColsB)];
-	}
-	C[toN(row, col, numColsC)] = sum;
-}
-
-int* readMatrix(char* path, int* numRows, int* numCols) {
-	FILE* file = fopen(path, "r");
-	if(!file) {
-		fprintf(stderr, "Could not open input file %s\n", path);
-		exit(-1);
-	}
-	fscanf(file, "LINHAS = %d ", numRows);
-	fscanf(file, "COLUNAS = %d", numCols);
-	int size = *numRows * *numCols;
-	int *M = malloc(sizeof(int) * size);
-	int n;
-	for(n = 0; n < size; n++) {
-		fscanf(file, "%d", &M[n]);
-	}
-	fclose(file);
-	return M;
-}
-
-void writeMatrix(char* path, int* M, int numRows, int numCols) {
-	FILE* file = fopen(path, "w");
-	if(!file) {
-		fprintf(stderr, "Could not open output file %s\n", path);
-		exit(-1);
-	}
-	printMatrix(file, M, numRows, numCols);
-	fclose(file);
-}
-
-void printMatrix(FILE* file, int *M, int numRows, int numCols) {
-	fprintf(file, "LINHAS = %d\nCOLUNAS = %d\n", numRows, numCols);
-	int i, j;
-	for (i = 0; i < numRows; ++i) {
-		for (j = 0; j < numCols; ++j)
-			fprintf(file, "%d ", M[toN(i, j, numCols)]);
-		fprintf(file, "\n");
-	}
-}
 
 void *doWork(void *args) {
 	//int id = (long) args;
 	//printf("Thread %d started.\n", id);
 	int row = (long) args;
-	while(row < numRowsC) {
+	while(row < C->numRows){
 		//printf("Thread %d multiplying row %d\n", id, row);
-		multiplyRow(row);
+		multiplyRow(C, A, B, row);
 		row += numThreads;
 	}
 	return NULL;
