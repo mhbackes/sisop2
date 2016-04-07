15aa86e86884b77924667d576a860c3d6a024313
gutoblaas <gutoblaas@gmail.com>
2016-04-07 19:02:26 -0300

    results first benchmark

--- trabalho1/solve-all.sh
+++ trabalho1/solve-all.sh
@@ -7,6 +7,8 @@
 HOME=`pwd`
 TESTS_DIR=${HOME}/test
 RESULTS=${HOME}/results
+RES_THREAD=${RESULTS}/thread
+RES_FORK=${RESULTS}/fork
 
 TIMEFORMAT=%R
 
@@ -16,20 +18,34 @@ numOfThreads='2
 
 if [ ! -d "${RESULTS}" ]; then
     mkdir ${RESULTS}
+    mkdir ${RES_THREAD}
+    mkdir ${RES_FORK}
 fi
-
-echo size time > ${RESULTS}/results.log
 
 for teste in ${TESTS_DIR}/*txt; do
     testName=`echo $teste | sed 's/.*\///'` #nome do arquivo teste 
     outputName=`basename $testName .txt`.sol #nome do arquivo de saída
     sizeMatrix=`basename $testName .txt | sed 's/x.*//'` #tamanho da matriz quadrada
     for thread in $numOfThreads; do
-	    echo -n  $(( $sizeMatrix * $sizeMatrix )) $thread" " >> ${RESULTS}/results.log 
+	    echo -n  $(( $sizeMatrix * $sizeMatrix )) $thread" " >> ${RES_THREAD}/results.log 
 	for exec in `seq 1 10`; do
-	    tempo=`(time ./matrix-thread $thread ${TESTS_DIR}/$testName ${TESTS_DIR}/$testName ${RESULTS}/$outputName) 2>&1`
-	    echo -n $tempo " " >> ${RESULTS}/results.log 
+	    tempo=`(time ./matrix-thread $thread ${TESTS_DIR}/$testName ${TESTS_DIR}/$testName ${RES_THREAD}/$outputName) 2>&1`
+	    echo -n $tempo " " >> ${RES_THREAD}/results.log 
 	done
-	echo " " >> ${RESULTS}/results.log 
+	echo " " >> ${RES_THREAD}/results.log 
+    done
+done
+
+for teste in ${TESTS_DIR}/*txt; do
+    testName=`echo $teste | sed 's/.*\///'` #nome do arquivo teste 
+    outputName=`basename $testName .txt`.sol #nome do arquivo de saída
+    sizeMatrix=`basename $testName .txt | sed 's/x.*//'` #tamanho da matriz quadrada
+    for thread in $numOfThreads; do
+	    echo -n  $(( $sizeMatrix * $sizeMatrix )) $thread" " >> ${RES_FORK}/results.log 
+	for exec in `seq 1 10`; do
+	    tempo=`(time ./matrix-thread $thread ${TESTS_DIR}/$testName ${TESTS_DIR}/$testName ${RES_FORK}/$outputName) 2>&1`
+	    echo -n $tempo " " >> ${RES_FORK}/results.log 
+	done
+	echo " " >> ${RES_FORK}/results.log 
     done
 done
